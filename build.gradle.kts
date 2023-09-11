f15fc59a3ca9d088aed2926006007b7c0aa9a7eb
kiselev <Mikhail.Kiselev@adesso.de>
2023-09-11 18:00:40 +0200

    bump versions + user java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,19 @@
+plugins {
+    java
+    idea
+}
+
 description = "Simplified copy of Mockito to show, how Mockito works."
 
 allprojects {
     group = "io.github.mickle-ak.MockitoInside"
-    version = "1.0.0"
+    version = "1.0.1"
 }
 
 val junit5Version  = "5.10.0"
 val assertjVersion = "3.24.2"
 val lombokVersion  = "1.18.28"
-val mockitoVersion = "5.4.0"
+val mockitoVersion = "5.5.0"
 
 subprojects {
     repositories {
@@ -16,14 +21,15 @@ subprojects {
     }
 
     apply(plugin = "java")
-    the<JavaPluginExtension>().apply {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
 
     dependencies {
 
-        "implementation"("org.eclipse.jdt:org.eclipse.jdt.annotation:2.2.600")
+        "implementation"("org.eclipse.jdt:org.eclipse.jdt.annotation:2.2.700")
 
         "testImplementation"(platform("org.junit:junit-bom:$junit5Version"))
         "testImplementation"("org.junit.jupiter:junit-jupiter:$junit5Version")
@@ -35,6 +41,13 @@ subprojects {
         "testAnnotationProcessor"("org.projectlombok:lombok:$lombokVersion")
     }
 
+    apply(plugin = "idea")
+    idea {
+        module {
+            setDownloadJavadoc(true)
+            setDownloadSources(true)
+        }
+    }
 }
 
 project(":MyMocker") {
